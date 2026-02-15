84d4a7d6eca5e85bd7d6eb7d171e9f59d2e3158c
Bernardo Sampaio Floriano Machado <bernardosfmachado@hotmail.com>
2026-02-15 02:04:31 -0300

    removing body parser

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import bodyParser from 'body-parser';
+// import bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use('/presents/webhook', bodyParser.raw({ type: 'application/json' }));
-  app.use('/presents/webhook/v2', bodyParser.raw({ type: 'application/json' }));
-  app.use('/presents/webhook/v3', bodyParser.raw({ type: 'application/json' }));
+  // app.use('/presents/webhook', bodyParser.raw({ type: 'application/json' }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
