e76721d9762308220b7578889a6b22fc41e6b75c
Bernardo Sampaio Floriano Machado <bernardosfmachado@hotmail.com>
2026-02-15 02:09:42 -0300

    change axios to fetch

--- src/modules/presents/presents.controller.ts
+++ src/modules/presents/presents.controller.ts
@@ -134,14 +134,22 @@ export class PresentsController {
       return { received: true };
     }
 
-    const { data: payment } = await axios.get(
+    // const { data: payment } = await axios.get(
+    //   `https://api.mercadopago.com/v1/payments/${paymentId}`,
+    //   {
+    //     headers: {
+    //       Authorization: `Bearer ${process.env.MP_ACCESS_TOKEN}`,
+    //     },
+    //   },
+    // );
+    const payment = await fetch(
       `https://api.mercadopago.com/v1/payments/${paymentId}`,
       {
         headers: {
           Authorization: `Bearer ${process.env.MP_ACCESS_TOKEN}`,
         },
       },
-    );
+    ).then((res) => res.json());
 
     console.log('######### Webhook PAYMENT:', payment);
 
